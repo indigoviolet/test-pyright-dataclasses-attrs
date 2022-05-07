d91e5890e5c37c0aa3da5c4bb414c97aabd97805
Venky Iyer <indigoviolet@users.noreply.github.com>
2022-05-07 21:03:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+
+import attrs
+
+
+@dataclass
+class Parent:
+    foo: int = field(default=2)
+
+
+@attrs.define
+class Child(Parent):
+    bar: str
+
+if __name__ == '__main__':
+    c = Child('c')
+    print(repr(c))
